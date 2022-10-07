01e4db2fc7aaa67e052ef32a4e97be3cfbcc2b1a
Kuldeep Singh <kuldeepsingh99@gmail.com>
2022-10-07 12:03:22 +0200

    Add files via upload

---
+++ src/main/java/com/portal/openshift/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.portal.openshift.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@Value("${spring.application.name}")
+	public String appName;
+	
+	@GetMapping("/hello")
+	public String ping() {
+		
+		
+		return "Hello";
+	}
+	
+	@GetMapping("/getapp-name")
+	public String getName() {
+		
+		
+		return appName;
+	}
+	
+}
